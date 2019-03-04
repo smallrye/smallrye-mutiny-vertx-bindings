7a481a55e7031382a9fbbcee3db2e61bbe0d0944
Clement Escoffier <clement.escoffier@gmail.com>
2019-03-04 21:12:43 +0100

    Copy the Vert.x Axle clients

---
+++ README.md
@@ -0,0 +1,17 @@
+# SmallRye Reactive Utilities
+
+This contains a set of modules helping the development of reactive applications in SmallRye 
+
+## Vert.x Axle Clients
+
+This module delivers Vert.x client using the _Axle_ API. 
+
+The _Axle_ API is based on `CompletionStage` and `Publisher`.
+
+## Build
+
+`mvn clean install`
+
+## Release
+
+`mvn release:prepare release:perform`
---
+++ vertx-axle-clients/vertx-axle-redis-client/src/test/java/io/vertx/axle/redis/RedisClientTest.java
@@ -0,0 +1,47 @@
+package io.vertx.axle.redis;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.entry;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.testcontainers.containers.GenericContainer;
+
+import io.vertx.axle.core.Vertx;
+import io.vertx.core.json.JsonObject;
+import io.vertx.redis.RedisOptions;
+
+public class RedisClientTest {
+
+    @Rule
+    public GenericContainer container = new GenericContainer("redis")
+            .withExposedPorts(6379);
+
+    private Vertx vertx;
+
+    @Before
+    public void setUp() {
+        vertx = Vertx.vertx();
+        assertThat(vertx).isNotNull();
+    }
+
+    @After
+    public void tearDown() {
+        vertx.close();
+    }
+
+    @Test
+    public void testAxleAPI() {
+        RedisClient client = RedisClient.create(vertx, new RedisOptions()
+                .setPort(container.getMappedPort(6379))
+                .setHost(container.getContainerIpAddress()));
+
+        JsonObject object = client.hset("book", "title", "The Hobbit")
+                .thenCompose(x -> client.hgetall("book"))
+                .toCompletableFuture()
+                .join();
+        assertThat(object).contains(entry("title", "The Hobbit"));
+    }
+}
