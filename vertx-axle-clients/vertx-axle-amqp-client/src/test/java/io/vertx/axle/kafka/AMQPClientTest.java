3295114b06852171588171938546946bf61d5acb
Clement Escoffier <clement.escoffier@gmail.com>
2019-05-11 13:20:11 +0200

    add the AMQP client module

---
+++ vertx-axle-clients/vertx-axle-amqp-client/src/test/java/io/vertx/axle/kafka/AMQPClientTest.java
@@ -0,0 +1,60 @@
+package io.vertx.axle.kafka;
+
+import io.vertx.axle.core.Vertx;
+import io.vertx.axle.ext.amqp.AmqpClient;
+import io.vertx.axle.ext.amqp.AmqpMessage;
+import io.vertx.ext.amqp.AmqpClientOptions;
+import org.eclipse.microprofile.reactive.streams.operators.PublisherBuilder;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.testcontainers.containers.GenericContainer;
+
+import java.util.Optional;
+import java.util.UUID;
+import java.util.concurrent.CompletionStage;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AMQPClientTest {
+
+    @Rule
+    public GenericContainer container = new GenericContainer("vromero/activemq-artemis")
+            .withExposedPorts(5672);
+
+    private Vertx vertx;
+
+    @Before
+    public void setUp() {
+        vertx = Vertx.vertx();
+        assertThat(vertx).isNotNull();
+    }
+
+    @After
+    public void tearDown() {
+        vertx.close();
+    }
+
+    @Test
+    public void testAxleAPI() {
+        AmqpClientOptions options = new AmqpClientOptions()
+                .setHost("localhost")
+                .setPort(container.getMappedPort(5672))
+                .setUsername("artemis")
+                .setPassword("simetraehcapa");
+
+        AmqpClient client = AmqpClient.create(vertx, options);
+        PublisherBuilder<AmqpMessage> stream = client.createReceiver("my-address").thenApply(receiver -> receiver.toPublisherBuilder()).toCompletableFuture().join();
+        CompletionStage<Optional<String>> result = stream
+                .map(AmqpMessage::bodyAsString)
+                .findFirst().run();
+
+        String payload = UUID.randomUUID().toString();
+        client.createSender("my-address").thenApply(sender -> sender.write(AmqpMessage.create().withBody(payload).build()));
+
+        Optional<String> optional = result.toCompletableFuture().join();
+        assertThat(optional).contains(payload);
+
+    }
+}
