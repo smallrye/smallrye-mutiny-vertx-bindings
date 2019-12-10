6cedc3298322faa4cfd03350a5d15ef7f88fb2bc
Clement Escoffier <clement.escoffier@gmail.com>
2019-12-10 14:27:57 +0100

    Generation of the Vert.x Web Client API

---
+++ vertx-mutiny-clients/vertx-mutiny-web-client/src/test/java/io/vertx/mutiny/web/WebClientTest.java
@@ -0,0 +1,53 @@
+package io.vertx.mutiny.web;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.testcontainers.containers.BindMode;
+import org.testcontainers.containers.GenericContainer;
+
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.client.WebClientOptions;
+import io.vertx.mutiny.core.Vertx;
+import io.vertx.mutiny.ext.web.client.HttpResponse;
+import io.vertx.mutiny.ext.web.client.WebClient;
+
+public class WebClientTest {
+
+    @Rule
+    public GenericContainer container = new GenericContainer("kennethreitz/httpbin")
+            .withExposedPorts(80)
+            .withFileSystemBind("target", "/tmp/fakemail", BindMode.READ_WRITE);
+
+    private Vertx vertx;
+
+    @Before
+    public void setUp() {
+        vertx = Vertx.vertx();
+        assertThat(vertx, is(notNullValue()));
+    }
+
+    @After
+    public void tearDown() {
+        vertx.close();
+    }
+
+    @Test
+    public void testWebClient() {
+        WebClient client = WebClient.create(vertx, new WebClientOptions()
+                .setDefaultPort(container.getMappedPort(80))
+                .setDefaultHost(container.getContainerIpAddress()));
+        assertThat(client, is(notNullValue()));
+
+        JsonObject object = client.get("/get?msg=hello").send()
+                .subscribeAsCompletionStage()
+                .thenApply(HttpResponse::bodyAsJsonObject)
+                .toCompletableFuture().join();
+        assertThat(object.getJsonObject("args").getString("msg"), is("hello"));
+    }
+}
