374fc6ba7b658edf9b9757232ca8181a1f315052
Clement Escoffier <clement.escoffier@gmail.com>
2019-12-10 14:27:47 +0100

    Generation of the Vert.x PostGreSQL (the vert.x one) Client API

---
+++ vertx-mutiny-clients/vertx-mutiny-pg-client/src/test/java/io/vertx/mutiny/postgresql/PostGreSQLClientTest.java
@@ -0,0 +1,51 @@
+package io.vertx.mutiny.postgresql;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+import io.vertx.mutiny.core.Vertx;
+import io.vertx.mutiny.pgclient.PgPool;
+import io.vertx.mutiny.sqlclient.Pool;
+import io.vertx.mutiny.sqlclient.RowSet;
+import io.vertx.pgclient.PgConnectOptions;
+import io.vertx.sqlclient.PoolOptions;
+
+public class PostGreSQLClientTest {
+
+    @Rule
+    public PostgreSQLContainer container = new PostgreSQLContainer();
+
+    private Vertx vertx;
+
+    @Before
+    public void setUp() {
+        vertx = Vertx.vertx();
+        assertThat(vertx).isNotNull();
+    }
+
+    @After
+    public void tearDown() {
+        vertx.close();
+    }
+
+    @Test
+    public void testMutinyAPI() {
+        PgConnectOptions options = new PgConnectOptions()
+                .setPort(container.getMappedPort(5432))
+                .setHost(container.getContainerIpAddress())
+                .setDatabase(container.getDatabaseName())
+                .setUser(container.getUsername())
+                .setPassword(container.getPassword());
+
+        Pool client = PgPool.pool(vertx, options, new PoolOptions().setMaxSize(5));
+
+        RowSet join = client.query("SELECT 1").subscribeAsCompletionStage().join();
+        assertThat(join).isNotNull();
+        assertThat(join.size()).isEqualTo(1);
+    }
+}
