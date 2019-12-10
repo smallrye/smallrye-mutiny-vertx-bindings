e1d8bf5ca04ea389350aecfb6421468c4992a55e
Clement Escoffier <clement.escoffier@gmail.com>
2019-12-10 14:25:11 +0100

    Generation of the Vert.x AMQP Client API

---
+++ vertx-mutiny-clients/vertx-mutiny-amqp-client/src/test/java/io/vertx/mutiny/amqp/AMQPClientTest.java
@@ -0,0 +1,63 @@
+package io.vertx.mutiny.amqp;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.testcontainers.containers.GenericContainer;
+
+import io.smallrye.mutiny.Multi;
+import io.smallrye.mutiny.Uni;
+import io.vertx.amqp.AmqpClientOptions;
+import io.vertx.mutiny.core.Vertx;
+
+public class AMQPClientTest {
+
+    @Rule
+    public GenericContainer container = new GenericContainer("vromero/activemq-artemis")
+            .withExposedPorts(5672);
+
+    private Vertx vertx;
+
+    @Before
+    public void setUp() {
+        vertx = Vertx.vertx();
+        assertThat(vertx).isNotNull();
+    }
+
+    @After
+    public void tearDown() {
+        vertx.close();
+    }
+
+    @Test
+    public void testMutinyAPI() {
+        String payload = UUID.randomUUID().toString();
+
+        AmqpClientOptions options = new AmqpClientOptions()
+                .setHost("localhost")
+                .setPort(container.getMappedPort(5672))
+                .setUsername("artemis")
+                .setPassword("simetraehcapa");
+
+        AmqpClient client = AmqpClient.create(vertx, options);
+        Multi<AmqpMessage> stream = client.createReceiver("my-address")
+                .onItem().apply(AmqpReceiver::toMulti)
+                .await().indefinitely();
+
+        Uni<AmqpMessage> first = stream.collectItems().first();
+
+        client.createSender("my-address")
+                .onItem().produceUni(sender -> sender.write(AmqpMessage.create().withBody(payload).build()))
+                .await().indefinitely();
+
+        Optional<String> optional = first.map(AmqpMessage::bodyAsString).await().asOptional().indefinitely();
+        assertThat(optional).contains(payload);
+
+    }
+}
