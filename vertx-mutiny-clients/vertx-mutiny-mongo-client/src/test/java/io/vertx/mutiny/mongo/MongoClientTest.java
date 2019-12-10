25689b2a0fba70b35273a64a1d7f6690420f7ad7
Clement Escoffier <clement.escoffier@gmail.com>
2019-12-10 14:26:27 +0100

    Generation of the Vert.x Mongo Client API

---
+++ vertx-mutiny-clients/vertx-mutiny-mongo-client/src/test/java/io/vertx/mutiny/mongo/MongoClientTest.java
@@ -0,0 +1,53 @@
+package io.vertx.mutiny.mongo;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.testcontainers.containers.GenericContainer;
+
+import io.vertx.core.json.JsonObject;
+import io.vertx.mutiny.core.Vertx;
+import io.vertx.mutiny.ext.mongo.MongoClient;
+
+public class MongoClientTest {
+
+    @Rule
+    public GenericContainer container = new GenericContainer("mongo")
+            .withExposedPorts(27017);
+
+    private Vertx vertx;
+
+    @Before
+    public void setUp() {
+        vertx = Vertx.vertx();
+        assertThat(vertx).isNotNull();
+    }
+
+    @After
+    public void tearDown() {
+        vertx.close();
+    }
+
+    @Test
+    public void testmutinyAPI() {
+        MongoClient client = MongoClient.createShared(vertx, new JsonObject()
+                .put("db_name", "mutiny-test")
+                .put("connection_string", "mongodb://" + container.getContainerIpAddress()
+                        + ":" + container.getMappedPort(27017)));
+
+        JsonObject document = new JsonObject().put("title", "The Hobbit");
+        List<JsonObject> list = client.save("books", document)
+                .onItem().produceUni(x -> client.find("books", new JsonObject().put("title", "The Hobbit")))
+                .subscribeAsCompletionStage()
+                .toCompletableFuture()
+                .join();
+
+        assertThat(list).hasSize(1)
+                .allMatch(json -> json.getString("title").equalsIgnoreCase("The Hobbit"));
+    }
+}
