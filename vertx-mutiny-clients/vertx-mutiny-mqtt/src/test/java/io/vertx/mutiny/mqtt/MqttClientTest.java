98d752fad1f81a74240b515739b18f60f8274b86
Clement Escoffier <clement.escoffier@gmail.com>
2020-03-08 10:11:48 +0100

    Add support for the Vert.x MQTT client and server

---
+++ vertx-mutiny-clients/vertx-mutiny-mqtt/src/test/java/io/vertx/mutiny/mqtt/MqttClientTest.java
@@ -0,0 +1,44 @@
+package io.vertx.mutiny.mqtt;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
+
+import io.vertx.mutiny.core.Vertx;
+import io.vertx.mutiny.mqtt.messages.MqttConnAckMessage;
+
+public class MqttClientTest {
+
+    @ClassRule
+    public static GenericContainer mosquitto = new GenericContainer<>("eclipse-mosquitto:1.6.7")
+            .withExposedPorts(1883)
+            .waitingFor(Wait.forLogMessage(".*listen socket on port 1883.*\\n", 2));
+    private Vertx vertx;
+
+    @Before
+    public void setUp() {
+        vertx = Vertx.vertx();
+        assertThat(vertx, is(notNullValue()));
+    }
+
+    @After
+    public void tearDown() {
+        vertx.closeAndAwait();
+    }
+
+    @Test
+    public void test() {
+        MqttConnAckMessage connection = MqttClient.create(vertx)
+                .connect(mosquitto.getMappedPort(1883), mosquitto.getContainerIpAddress(), null)
+                .await().indefinitely();
+        assertThat(connection, is(notNullValue()));
+    }
+
+}
---
+++ vertx-mutiny-clients/vertx-mutiny-mqtt/src/test/java/io/vertx/mutiny/mqtt/MqttServerTest.java
@@ -0,0 +1,36 @@
+package io.vertx.mutiny.mqtt;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import io.vertx.mqtt.MqttServerOptions;
+import io.vertx.mutiny.core.Vertx;
+
+public class MqttServerTest {
+
+    private Vertx vertx;
+
+    @Before
+    public void setUp() {
+        vertx = Vertx.vertx();
+        assertThat(vertx, is(notNullValue()));
+    }
+
+    @After
+    public void tearDown() {
+        vertx.closeAndAwait();
+    }
+
+    @Test
+    public void test() {
+        MqttServer server = MqttServer.create(vertx, new MqttServerOptions()
+                .setPort(0).setHost("0.0.0.0")).listen().await().indefinitely();
+        assertThat(server, is(notNullValue()));
+        assertThat(server.actualPort(), is(not(0)));
+    }
+
+}
