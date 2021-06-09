a0214d31f437702a5508e1562dbd252ff6868226
Julien Ponge <jponge@redhat.com>
2021-06-09 11:07:43 +0200

    Full website deployment

--- .build/render-documentation.sh
+++ .build/render-documentation.sh
@@ -2,8 +2,8 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-# mvn -f vertx-mutiny-clients/pom.xml pre-site
-# cp -R vertx-mutiny-clients/target/site/apidocs docs/
+mvn -f vertx-mutiny-clients/pom.xml pre-site
+cp -R vertx-mutiny-clients/target/site/apidocs docs/
 
 PROJECT_VERSION=$(cat .github/project.yml | yq eval '.release.current-version' -)
 mike deploy --push --force --update-aliases $PROJECT_VERSION latest
