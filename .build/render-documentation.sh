9416ab9ac3d16108aa5003d3a776d25ee483df40
Julien Ponge <jponge@redhat.com>
2021-06-17 10:22:52 +0200

    Do not push force MkDocs (erases older versions)

--- .build/render-documentation.sh
+++ .build/render-documentation.sh
@@ -6,5 +6,5 @@ mvn -f vertx-mutiny-clients/pom.xml pre-site
 cp -R vertx-mutiny-clients/target/site/apidocs docs/
 
 PROJECT_VERSION=$(cat .github/project.yml | yq eval '.release.current-version' -)
-mike deploy --push --force --update-aliases $PROJECT_VERSION latest
-mike set-default --push --force latest
+mike deploy --push --update-aliases $PROJECT_VERSION latest
+mike set-default --push latest
