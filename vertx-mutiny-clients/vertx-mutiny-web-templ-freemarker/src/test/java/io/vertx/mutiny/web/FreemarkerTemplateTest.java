f5a57bf06e5dd3bfb59c9e106a15f1056779b0b4
anavarr <38165646+anavarr@users.noreply.github.com>
2026-01-14 17:37:50 +0100

    feat: port all vertx-web-templ except jade (#1234)

--- vertx-mutiny-clients/vertx-mutiny-web-templ-freemarker/src/test/java/io/vertx/mutiny/web/FreemarkerTemplateTest.java
+++ vertx-mutiny-clients/vertx-mutiny-web-templ-freemarker/src/test/java/io/vertx/mutiny/web/FreemarkerTemplateTest.java
@@ -2,26 +2,26 @@ package io.vertx.mutiny.web;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
+import io.vertx.core.buffer.Buffer;
 import io.vertx.core.json.JsonObject;
 import io.vertx.mutiny.core.Vertx;
-import io.vertx.mutiny.core.buffer.Buffer;
 import io.vertx.mutiny.ext.web.templ.freemarker.FreeMarkerTemplateEngine;
 
 public class FreemarkerTemplateTest {
 
-    private Vertx vertx;
+    static private Vertx vertx;
 
-    @Before
-    public void setUp() {
+    @BeforeAll
+    public static void setUp() {
         vertx = Vertx.vertx();
     }
 
-    @After
-    public void tearDown() {
+    @AfterAll
+    public static void tearDown() {
         vertx.closeAndAwait();
     }
 
