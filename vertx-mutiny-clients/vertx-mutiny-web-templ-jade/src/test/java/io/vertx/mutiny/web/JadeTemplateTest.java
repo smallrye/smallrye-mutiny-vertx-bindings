76b0297ffd4d293a021b756ab04dbcf08bb7ea74
anavarr <artnavarro@protonmail.com>
2026-01-14 16:33:19 +0100

    port all vertx-web-templ except jade

--- vertx-mutiny-clients/vertx-mutiny-web-templ-jade/src/test/java/io/vertx/mutiny/web/JadeTemplateTest.java
+++ vertx-mutiny-clients/vertx-mutiny-web-templ-jade/src/test/java/io/vertx/mutiny/web/JadeTemplateTest.java
@@ -2,26 +2,25 @@ package io.vertx.mutiny.web;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-
 import io.vertx.core.json.JsonObject;
 import io.vertx.mutiny.core.Vertx;
-import io.vertx.mutiny.core.buffer.Buffer;
+import io.vertx.core.buffer.Buffer;
 import io.vertx.mutiny.ext.web.templ.jade.JadeTemplateEngine;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
 public class JadeTemplateTest {
 
-    private Vertx vertx;
+    static private Vertx vertx;
 
-    @Before
-    public void setUp() {
+    @BeforeAll
+    public static void setUp() {
         vertx = Vertx.vertx();
     }
 
-    @After
-    public void tearDown() {
+    @AfterAll
+    public static void tearDown() {
         vertx.closeAndAwait();
     }
 
